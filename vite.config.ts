c8a1911f2c434edf4f638d104fee3821672742e1
suphicharac-netizen <suphicha.rac@gmail.com>
2026-09-15 10:31:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/sila-plan-2571-2575/', plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     host: '0.0.0.0'
